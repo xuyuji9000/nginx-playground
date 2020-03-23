c8c3a0f09116ea9013f7ba4785f2aac37b3b3cae
Karl Xu <karl.yogi.xu@gmail.com>
2020-03-24 00:33:33 +0800

    add nodejs for load balance

---
+++ load-balance/nodejs/index.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000||process.env.PORT;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end(`Process ID: ${process.pid}`);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
